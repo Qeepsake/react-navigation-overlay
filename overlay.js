25debbcbc42f99671e3201e064c54ed0587e15d5
Rodrigo Vieira <rodriigovieira@gmail.com>
2020-12-17 01:43:32 -0300

    exporting only one file

--- overlay.js
+++ overlay.js
@@ -3,14 +3,45 @@
 /** Constants */
 let OverlayRef = null;
 
-export const Overlay = {
-    register: (ref) => {
-      OverlayRef = ref.current;
-    },
-    show: (component, props) => {
-      OverlayRef.show(component, props);
+const Overlay = {
+  register: (ref) => {
+    OverlayRef = ref.current;
+  },
+  show: (component, props) => {
+    OverlayRef.show(component, props);
+  },
+  close: () => {
+    OverlayRef.close();
+  }
+};
+
+Overlay.show = () => {
+  const thisRef = React.createRef();
+  const ComponentJsxRef = useRef(null);
+  const [componentPropsRef, setComponentPropsRef] = useState({});
+  /** Effects */
+  useEffect(() => Overlay.register(thisRef), [thisRef]);
+  useImperativeHandle(thisRef, () => ({
+    show: (component, props = {}) => {
+      ComponentJsxRef.current = component;
+      setComponentPropsRef(props);
     },
     close: () => {
-      OverlayRef.close();
-    }
+      ComponentJsxRef.current = null;
+      setComponentPropsRef({});
+    },
+  }));
+
+
+  if (!_isNil(ComponentJsxRef.current)) {
+    return (
+      <View style={{ flex: null }}>
+        <ComponentJsxRef.current {...componentPropsRef} />
+      </View>
+    );
+  }
+
+  return null;
 };
+
+export { Overlay }
