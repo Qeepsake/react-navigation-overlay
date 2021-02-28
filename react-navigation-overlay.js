289657d2838ded85d7ab7b001086dbb7ecc8ed60
lukebrandonfarrell <lukebrandonfarrell@gmail.com>
2021-02-28 12:20:22 +0000

    Fixed the modal implementation with the overlay using react-native-overlay

--- react-navigation-overlay.js
+++ react-navigation-overlay.js
@@ -1,38 +1,47 @@
-/**
- * @author Qeepsake, Inc.
- * @description
- */
-
 import React, { useEffect, useImperativeHandle, useRef, useState } from "react";
-import _isNil from "lodash/isNil";
+import { InteractionManager } from "react-native";
 import { Overlay } from "./overlay";
-import { View } from "react-native-animatable";
+import Modal from "react-native-modal";
 
-export const ReactNavigationOverlay = () => {
+export const ReactNavigationOverlay = ({ style, animationIn="slideInUp", animationOut="slideOutDown", onModalHide, ...props }) => {
     const thisRef = React.createRef();
     const ComponentJsxRef = useRef(null);
     const [componentPropsRef, setComponentPropsRef] = useState({});
+    const [isVisible, setIsVisible] = useState(false);
     /** Effects */
     useEffect(() => Overlay.register(thisRef), [thisRef]);
     useImperativeHandle(thisRef, () => ({
         show: (component, props = {}) => {
-            ComponentJsxRef.current = component;
-            setComponentPropsRef(props);
+            InteractionManager.runAfterInteractions(() => {
+                ComponentJsxRef.current = component;
+                setComponentPropsRef(props);
+                setIsVisible(true)
+            });            
         },
         close: () => {
-            ComponentJsxRef.current = null;
-            setComponentPropsRef({});
+            setIsVisible(false);
         },
     }));
    
 
-    if(!_isNil(ComponentJsxRef.current)){
+    if(ComponentJsxRef.current) {
         return (
-            <View style={{ flex: null }}>
-               <ComponentJsxRef.current {...componentPropsRef} />
-            </View>
+            <Modal
+                isVisible={isVisible}
+                style={[{ flex: 1, margin: 0 }, style]}
+                animationIn={animationIn}
+                animationOut={animationOut}
+                onModalHide={() => {
+                    ComponentJsxRef.current = null;
+                    setComponentPropsRef({});
+                    if(onModalHide) onModalHide();
+                }}
+                {...props}
+            >
+                <ComponentJsxRef.current {...componentPropsRef} />
+            </Modal>
         );
     }
-    
+
     return null;
 };
